2db3bb30db60483b9faeb8b1e539059f4b72ff26
Sean Modd <63374894+seanmodd@users.noreply.github.com>
2021-04-05 13:27:38 -0700

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import { Heading } from '@chakra-ui/layout'
+import type { TypeA } from '../types'
+import { GoogleFonts } from 'next-google-fonts';
+const Example: TypeA = {
+  name: 'next',
+}
+
+export default function Home() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <h1>
+        <p>{Example.name}</p>
+        <GoogleFonts href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" />
+        <Heading fontFamily="Poppins">Here</Heading>
+      </h1>
+    </div>
+  )
+}
